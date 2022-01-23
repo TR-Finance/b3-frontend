773f56baab6ab7d92a866a9c67a04d4dbb9df472
Ryan Lavin <rlavin@usc.edu>
2022-01-23 12:13:41 -0500

    Partial option select functionality

--- src/components/stake/NetworksMenu.tsx
+++ src/components/stake/NetworksMenu.tsx
@@ -1,27 +1,42 @@
 import React, {useState} from 'react';
-import {Box, Select, Center, Heading, Icon} from "@chakra-ui/react";
-import ethLogo from '../../assets/logos/eth.svg';
-import arbLogo from '../../assets/logos/arbitrum.svg';
+import {Box, Center, Heading, Icon} from "@chakra-ui/react";
+import Select, {components} from 'react-select';
+import {ReactComponent as ETHLogo} from '../../assets/logos/eth.svg';
+import {ReactComponent as ARBLogo} from '../../assets/logos/arbitrum.svg';
 
 const NetworksMenu = () => {
 
     const options = [
         {
-            ticker: 'ETH',
-            logo: <div><img src={ethLogo} height="30px" width="30px" alt=''/>ETH </div>
+            label: <div style={{textAlign: 'right', justifyContent: 'right'}}>ETH</div>,
+            logo: <ETHLogo style={{maxWidth: 25, maxHeight: 25}} />,
         },
         {
-            ticker: 'ARB',
-            logo: <div><img src={arbLogo} height="30px" width="30px" alt=''/>ARB </div>
+            label: <div style={{textAlign: 'right', justifyContent: 'right'}}>ARB</div>,
+            logo: <ARBLogo style={{maxWidth: 25, maxHeight: 25}}/>,
         }
     ];
 
+    const { Option } = components;
+    /* eslint-disable react/prop-types */
+    const listOptions = (props:any) => (
+        /* eslint-disable react/jsx-props-no-spreading */
+        <Option {...props} style={{minWidth: 450, minHeight:450}}>
+            {props.data.logo}
+            {props.data.value}
+        </Option>
+    );
+
     return (
         <Box m={25}>
-            <Center flexDir="row" width={250}>
-                <Select options={options} />
+            <Center flexDir="row" width={650}>
+                <Select
+                    defaultValue={options[0]}
+                    options={options}
+                    components={{ Option: listOptions }}
+                />
                 <Heading m={15}>on</Heading>
-                <Select size='lg' />
+                <Select />
             </Center>
         </Box>
     );
--- src/pages/Stake/index.tsx
+++ src/pages/Stake/index.tsx
@@ -10,5 +10,4 @@ const Stake = () =>
             </Center>
         </Box>
 
-
 export default Stake;
