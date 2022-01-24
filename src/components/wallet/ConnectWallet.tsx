c4c199e8ddafe44a94b342db52f8aa0cada60ce5
Ryan Lavin <rlavin@usc.edu>
2022-01-24 10:19:15 -0800

    Added wallet balance to nav

--- src/components/wallet/ConnectWallet.tsx
+++ src/components/wallet/ConnectWallet.tsx
@@ -3,11 +3,15 @@ import { Web3Provider } from '@ethersproject/providers';
 
 import { Box, Button, Tag, TagLabel, TagLeftIcon } from '@chakra-ui/react';
 import { MdOutlineCheckCircle } from 'react-icons/md';
+import {utils} from "ethers";
 
 import { injectedConnector } from '../../connectors';
+import {useEtherBalance} from "../../state/queries";
 
 const ConnectWallet = () => {
   const { chainId, account, activate, active, error } = useWeb3React<Web3Provider>();
+
+  const { data: etherBalance, isLoading: etherBalanceLoading } = useEtherBalance(chainId || 0, account || '');
 
   const onClick = () => {
     activate(injectedConnector);
@@ -36,21 +40,25 @@ const ConnectWallet = () => {
     <Box w="100%" p={1} display="flex" justifyContent="flex-end">
       {active && (
         <>
-          <Tag size="md" key="connected network" variant="subtle" colorScheme="cyan" borderRadius="full">
+          <Tag size="md" key="connected network" variant="subtle" colorScheme="cyan" borderRadius="full" mr={1}>
             {chainIdToNetwork()}
           </Tag>
-          <Tag size="md" key="connected wallet" variant="subtle" colorScheme="cyan" borderRadius="full">
+          <Tag size="md" key="wallet balance" variant="subtle" colorScheme="cyan" borderRadius="full">
+            {parseFloat(utils.formatEther(etherBalance || 0)).toFixed(4)} ETH
+            <Tag size="md" key="connected wallet" variant="subtle" colorScheme="blue" borderRadius="full">
             <TagLeftIcon boxSize="15px" as={MdOutlineCheckCircle} color="green" />
             <TagLabel>
               {account?.substring(0, 6)}...{account?.substring(account.length - 4, account.length)}
             </TagLabel>
           </Tag>
+          </Tag>
+
         </>
       )}
       {!active && error && error instanceof UnsupportedChainIdError && <>Unsupported Network</>}
       {!active && !(error && error instanceof UnsupportedChainIdError) && (
         <Button colorScheme="blue" onClick={onClick}>
-          Connect Metmask
+          Connect Metamask
         </Button>
       )}
     </Box>
